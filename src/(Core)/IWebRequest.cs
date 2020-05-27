eca3a539cd31d22119b13b1567f51328b0b39429
Pavel Sulimau <pavel.sulimau@gmail.com>
2020-05-27 17:18:44 +0300

    Add 'Body' get property to 'IWebRequest'

--- src/(Core)/IWebRequest.cs
+++ src/(Core)/IWebRequest.cs
@@ -25,38 +25,40 @@ namespace WebRequest.Elegant
         /// </summary>
         HttpMethod HttpMethod { get; }
 
+        IJsonObject Body { get; }
+
         /// <summary>
         /// Executes the web request.
         /// </summary>
         /// <returns>Return response.</returns>
-        Task<HttpResponseMessage> GetAsync();
+        Task<HttpResponseMessage> GetResponseAsync();
 
         /// <summary>
         /// Creates a new web request with specified path.
         /// </summary>
         /// <param name="uri">Target web uri.</param>
         /// <returns>New web request that configured with specified path.</returns>
-        IWebRequest Path(Uri uri);
+        IWebRequest WithPath(Uri uri);
 
         /// <summary>
         /// Creates a new web request with specified http method.
         /// </summary>
         /// <param name="method">Http method.</param>
         /// <returns>New web request that configured with specified http method.</returns>
-        IWebRequest Method(HttpMethod method);
+        IWebRequest WithMethod(HttpMethod method);
 
         /// <summary>
         /// Creates a new web request with specified query parameters.
         /// </summary>
         /// <param name="parameters">Dictionary with query parameters.</param>
         /// <returns>New web request that configured with specified query parameters.</returns>
-        IWebRequest QueryParams(Dictionary<string, string> parameters);
+        IWebRequest WithQueryParams(Dictionary<string, string> parameters);
 
         /// <summary>
         /// Creates a new web request with specified post body.
         /// </summary>
         /// <param name="postBody">The post body as json object.</param>
         /// <returns>New web request that is configured with specified post body parameter.</returns>
-        IWebRequest Body(IJsonObject postBody);
+        IWebRequest WithBody(IJsonObject postBody);
     }
 }
--- src/Extensions/WebRequestExtensions.cs
+++ src/Extensions/WebRequestExtensions.cs
@@ -4,23 +4,23 @@ namespace WebRequest.Elegant.Extensions
 {
     public static class WebRequestExtensions
     {
-        public static IWebRequest Path(this IWebRequest request, string url)
+        public static IWebRequest WithPath(this IWebRequest request, string url)
         {
-            return request.Path(new Uri(url));
+            return request.WithPath(new Uri(url));
         }
 
-        public static IWebRequest RelativePath(this IWebRequest request, string url)
+        public static IWebRequest WithRelativePath(this IWebRequest request, string url)
         {
-            if (IsDoubleDash(request, url))
+            if (HasDoubleSlash(request, url))
             {
                 url = url.TrimStart('/');
             }
-            return request.Path(new Uri(request.Uri.AbsoluteUri + url));
+            return request.WithPath(new Uri(request.Uri.AbsoluteUri + url));
         }
 
-        private static bool IsDoubleDash(IWebRequest request, string url)
+        private static bool HasDoubleSlash(IWebRequest request, string url)
         {
-            return request.Uri.AbsoluteUri[request.Uri.AbsoluteUri.Length - 1] == '/' 
+            return request.Uri.AbsoluteUri[request.Uri.AbsoluteUri.Length - 1] == '/'
                 && url[0] == '/';
         }
     }
